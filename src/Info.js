0a25a70e78d6c209a23433deed69ba619abe3550
carless7 <csh3@alumnes.udl.cat>
2025-03-22 20:43:27 +0100

    feat: added info page

---
+++ src/Info.js
@@ -0,0 +1,24 @@
+// src/Info.js
+import React from "react";
+
+function Info() {
+  return (
+    <div>
+      <h2>Informació del projecte</h2>
+      <p>Aquest és un Treball de Final de Grau realitzat per Carles Sànchez Hidalgo amb l'ajuda, mentoria i tutoria de Ferran Aran Domingo.</p>
+      <p>
+        Pots trobar més informació al següent enllaç:
+        <br />
+        <a
+          href="https://github.com/spotifai-tfg"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub Repo
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default Info;
